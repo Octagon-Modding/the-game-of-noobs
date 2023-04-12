8cea2a78e9dc212404ca0c1d22fadfb67864aa2e
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-04-12 17:09:38 +0100

    Git messed things up

---
+++ src/main/java/io/itch/awesomekalin/noob/block/DirtLeavesBlock.java
@@ -0,0 +1,62 @@
+
+package io.itch.awesomekalin.noob.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.itch.awesomekalin.noob.NoobModElements;
+
+@NoobModElements.ModElement.Tag
+public class DirtLeavesBlock extends NoobModElements.ModElement {
+	@ObjectHolder("noob:dirt_leaves")
+	public static final Block block = null;
+	public DirtLeavesBlock(NoobModElements instance) {
+		super(instance, 58);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(0.02f, 0.02f).lightValue(0).notSolid());
+			setRegistryName("dirt_leaves");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 1;
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 3;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/io/itch/awesomekalin/noob/block/DirtPlanksBlock.java
@@ -0,0 +1,63 @@
+
+package io.itch.awesomekalin.noob.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.itch.awesomekalin.noob.itemgroup.NoobTabItemGroup;
+import io.itch.awesomekalin.noob.NoobModElements;
+
+@NoobModElements.ModElement.Tag
+public class DirtPlanksBlock extends NoobModElements.ModElement {
+	@ObjectHolder("noob:dirt_planks")
+	public static final Block block = null;
+	public DirtPlanksBlock(NoobModElements instance) {
+		super(instance, 57);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(NoobTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(0.2f, 0.4754679577383341f).lightValue(0)
+					.harvestLevel(0).harvestTool(ToolType.AXE));
+			setRegistryName("dirt_planks");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 1;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
