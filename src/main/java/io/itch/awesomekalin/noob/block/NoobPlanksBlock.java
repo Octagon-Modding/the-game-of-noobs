0b5233eebd7ac434ebf7eab594a225d827053206
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-04-12 18:30:46 +0100

    Fixed textures and made wood types

---
+++ src/main/java/io/itch/awesomekalin/noob/block/NoobPlanksBlock.java
@@ -0,0 +1,65 @@
+
+package io.itch.awesomekalin.noob.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.itch.awesomekalin.noob.itemgroup.NoobTabItemGroup;
+import io.itch.awesomekalin.noob.NoobModElements;
+
+@NoobModElements.ModElement.Tag
+public class NoobPlanksBlock extends NoobModElements.ModElement {
+	@ObjectHolder("noob:noob_planks")
+	public static final Block block = null;
+
+	public NoobPlanksBlock(NoobModElements instance) {
+		super(instance, 81);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(NoobTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1f, 1.7230475324955523f).setLightLevel(s -> 0)
+					.harvestLevel(0).harvestTool(ToolType.AXE));
+			setRegistryName("noob_planks");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 3;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
