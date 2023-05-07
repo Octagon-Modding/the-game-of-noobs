9228c0529605784a222cadb6f19a93b5af6882cd
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-05-07 18:38:36 +0100

    Noob and Dirt Wood Tools

---
+++ src/main/java/io/itch/awesomekalin/noob/item/DirtShovelItem.java
@@ -0,0 +1,54 @@
+
+package io.itch.awesomekalin.noob.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import io.itch.awesomekalin.noob.itemgroup.NoobTabItemGroup;
+import io.itch.awesomekalin.noob.block.DirtPlanksBlock;
+import io.itch.awesomekalin.noob.NoobModElements;
+
+@NoobModElements.ModElement.Tag
+public class DirtShovelItem extends NoobModElements.ModElement {
+	@ObjectHolder("noob:dirt_wood_shovel")
+	public static final Item block = null;
+
+	public DirtShovelItem(NoobModElements instance) {
+		super(instance, 90);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ShovelItem(new IItemTier() {
+			public int getMaxUses() {
+				return 10;
+			}
+
+			public float getEfficiency() {
+				return 2f;
+			}
+
+			public float getAttackDamage() {
+				return -2f;
+			}
+
+			public int getHarvestLevel() {
+				return 0;
+			}
+
+			public int getEnchantability() {
+				return 1;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(DirtPlanksBlock.block));
+			}
+		}, 1, -3f, new Item.Properties().group(NoobTabItemGroup.tab)) {
+		}.setRegistryName("dirt_wood_shovel"));
+	}
+}
---
+++ src/main/java/io/itch/awesomekalin/noob/item/NoobWooPickaxeItem.java
@@ -0,0 +1,54 @@
+
+package io.itch.awesomekalin.noob.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import io.itch.awesomekalin.noob.itemgroup.NoobTabItemGroup;
+import io.itch.awesomekalin.noob.block.NoobPlanksBlock;
+import io.itch.awesomekalin.noob.NoobModElements;
+
+@NoobModElements.ModElement.Tag
+public class NoobWooPickaxeItem extends NoobModElements.ModElement {
+	@ObjectHolder("noob:noob_wood_pickaxe")
+	public static final Item block = null;
+
+	public NoobWooPickaxeItem(NoobModElements instance) {
+		super(instance, 157);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 26;
+			}
+
+			public float getEfficiency() {
+				return 2f;
+			}
+
+			public float getAttackDamage() {
+				return -2f;
+			}
+
+			public int getHarvestLevel() {
+				return 0;
+			}
+
+			public int getEnchantability() {
+				return 3;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(NoobPlanksBlock.block));
+			}
+		}, 1, -3f, new Item.Properties().group(NoobTabItemGroup.tab)) {
+		}.setRegistryName("noob_wood_pickaxe"));
+	}
+}
